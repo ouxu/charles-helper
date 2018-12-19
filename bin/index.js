6b8a3e738dea3813cfbcb52f9761c7194fd8806a
ouxu <out_xu@outlook.com>
2018-12-19 16:20:18 +0800

    chore: qrcode gen local

--- bin/index.js
+++ bin/index.js
@@ -2,52 +2,46 @@
 
 const os = require('os');
 const boxen = require('boxen');
-
-const { exec } = require('child_process');
+const qrcode = require('qrcode-generator');
 const { semble } = require('semble');
-const sslStr = require('../src/sslStr')
 
 const interfaces = os.networkInterfaces();
 
-const QR = "https://qc.now.sh/?"
+const QR = 'https://qc.now.sh/?';
+const QRTypeNumber = 3;
 
 const getNetworkAddress = () => {
-	for (const name of Object.keys(interfaces)) {
-		for (const interface of interfaces[name]) {
-			const {address, family, internal} = interface;
-			if (family === 'IPv4' && !internal) {
-				return address;
-			}
-		}
-	}
+  for (const name of Object.keys(interfaces)) {
+    for (const interface of interfaces[name]) {
+      const { address, family, internal } = interface;
+      if (family === 'IPv4' && !internal) {
+        return address;
+      }
+    }
+  }
+};
+
+const createQrcode = (text) => {
+  qrcode.stringToBytes = qrcode.stringToBytesFuncs['UTF-8'];
+  var qr = qrcode(QRTypeNumber, 'M');
+  qr.addData(text, 'Byte');
+  qr.make();
+  return qr.createASCII();
 };
 
 (function charlesHelper() {
   const ip = getNetworkAddress();
   const ssl = 'http://chls.pro/ssl';
 
-  const cmdStr4ip = `echo "${QR}${ip}" | curl -F-=\\<- qrenco.de`;
+  const ipQr = createQrcode(`${QR}${ip}`);
+  const sslQr = createQrcode(ssl);
 
-  exec(cmdStr4ip, (err, stdout) => {
-    if (err) {
-      const spiritualWords = semble([
-        [
-          boxen(`${ip}\n\nget QR code error`, { padding: 1, borderColor: 'green', align: 'center' }),
-          boxen(`${ssl}\n\n${sslStr}`, { borderColor: 'green', align: 'center' }),
-        ],
-      ]);
-      // eslint-disable-next-line no-console
-      console.log(spiritualWords);
-      return;
-    }
-
-    const spiritualWords = semble([
-      [
-        boxen(`quickly copy ${ip}\n\n${stdout}`, { borderColor: 'green', align: 'center' }),
-        boxen(`${ssl}\n\n${sslStr}`, { borderColor: 'green', align: 'center' }),
-      ],
-    ]);
-    // eslint-disable-next-line no-console
-    console.log(spiritualWords);
-  });
-}());
+  const spiritualWords = semble([
+    [
+      boxen(`quickly copy ${ip}\n\n${ipQr}`, { borderColor: 'green', align: 'center' }),
+      boxen(`${ssl}\n\n${sslQr}`, { borderColor: 'green', align: 'center' })
+    ]
+  ]);
+  // eslint-disable-next-line no-console
+  console.log(spiritualWords);
+})();
